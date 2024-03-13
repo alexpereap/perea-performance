c77f3253891c3c60a5449929ebc0076300c3f0ee
Alex Perea <ing.alexperea@gmail.com>
2024-03-13 17:45:21 -0500

    Added backend service

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('The backend is here')
+})
+
+app.get('/envs', (req, res) => {
+  res.send(process.env.RDS_HOST)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
