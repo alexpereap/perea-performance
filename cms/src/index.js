2f1f47b5c033927d78881d2e55ee51c68c1e9196
Alex Perea <ing.alexperea@gmail.com>
2024-03-18 19:02:56 -0500

    cms eslint, mvc structure

--- cms/src/index.js
+++ cms/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cms = require('../routes/cms.routes');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('CMS HOME');
-});
+// body parsers for requests
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// home handler
+app.use(cms);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`CMS app listening on port ${port}`);
 });
